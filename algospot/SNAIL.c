8c929e9b0b0079b98a997c37397b5841e0fdea4d
daeyab <33540416+daeyab@users.noreply.github.com>
2021-01-12 21:58:31 +0900

    Create SNAIL.c

---
+++ algospot/SNAIL.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<memory.h>
+
+int tc,n,m;
+
+//double len[1001];
+double dp[1001][2005];
+
+double solve(int days,int meter){
+	//n일 동안 m미터에 갈 확률
+	if(days==m)
+		return meter>=n ? 1.0:0.0;
+	double &ret=dp[days][meter];
+	if(ret!=-1.0)
+		return ret;
+	return ret=0.75*solve(days+1, meter+2)+0.25*solve(days+1,meter+1);
+}
+int main(){
+	scanf("%d",&tc);
+	while(tc--){
+		int i,j;
+		for(i=0;i<1001;i++){
+			for(j=0;j<2010;j++)
+				dp[i][j]=-1;
+				//실수는 memset으로 초기화하면 안된다! 
+		}
+		scanf("%d %d",&n,&m);
+		printf("%.8f\n",solve(0,0));
+	}
+}
