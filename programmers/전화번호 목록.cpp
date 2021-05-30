2b29fb57e9b4df4457c89941d21e0af52b53ea38
daeyab <33540416+daeyab@users.noreply.github.com>
2021-05-30 22:07:28 +0900

    Update 전화번호 목록.cpp

--- programmers/전화번호 목록.cpp
+++ programmers/전화번호 목록.cpp
@@ -4,19 +4,14 @@
 
 using namespace std;
 
-bool solution(vector<string> phone_book) { 
-    sort(phone_book.begin(),phone_book.end());
-    for(int i=0;i<phone_book.size();i++){
-        for(int j=i+1;j<phone_book.size();j++){
-            string cur = phone_book[i];
-            string com = phone_book[j];
-            if(cur.length()<com.length())
-                com=com.substr(0,cur.length());
-            else
-                cur= cur.substr(0,com.length());
-            if(com==cur)
-                return false;
-        }
+bool solution(vector<string> phone_book) {
+    sort(phone_book.begin(), phone_book.end());
+    for(int i = 0; i < phone_book.size()-1;i++){
+        string cur = phone_book[i];
+        string compare = phone_book[i+1];
+        if(cur.length() > compare.length()) cur = cur.substr(0, compare.length());
+        else compare = compare.substr(0, cur.length());
+        if(cur == compare) return false;
     }
     return true;
 }
