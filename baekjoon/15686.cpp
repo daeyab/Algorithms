fb3897ef7d80a1a3e19a6b1a44a7b2849b7b86d5
daeyab <daeyab@naver.com>
2021-06-05 00:09:04 +0900

    Add 치킨거리.cpp

---
+++ baekjoon/15686.cpp
@@ -0,0 +1,58 @@
+#include <math.h>
+#include <stdio.h>
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+struct posi {
+    int y, x;
+};
+
+int n, m, tp, res = 0x7fffffff;
+vector<posi> store, home;
+posi p;
+
+vector<posi> ls;
+
+void pick_store(int idx) {
+    if (ls.size() == m) {
+        // 다 채워진 경우
+        // for (auto a : ls) printf("-> %d %d \n", a.x, a.y);
+        // printf("\n");
+        int sum = 0;
+        for (auto h : home) {
+        int minVal = 100000;
+             for (auto s : ls) {
+                minVal = min(minVal, abs(h.x - s.x) + abs(h.y - s.y));
+            }
+            sum += minVal;
+        }
+        res = min(sum, res);
+        return;
+    } 
+
+        for (int i = idx; i < store.size(); i++) {
+            ls.push_back(store[i]);
+            pick_store(idx + 1);
+            ls.pop_back();
+        }
+    
+}
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+            scanf("%d", &tp);
+            if (tp == 1) {
+                p.x = x, p.y = y;
+                home.push_back(p);
+            } else if (tp == 2) {
+                p.x = x, p.y = y;
+                store.push_back(p);
+            }
+        }
+    }
+    pick_store(0);
+    printf("%d\n", res);
+}
